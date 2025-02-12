e60f39a7b237aa21240d1db821d9006b86d66e36
Eloise Holland <eloise.holland@Mac.Home>
2025-02-12 15:38:19 +0000

    adding tests for calculateTax

--- src/test/scala/TaxCalculatorSpec.scala
+++ src/test/scala/TaxCalculatorSpec.scala
@@ -8,11 +8,33 @@ class TaxCalculatorSpec extends AnyWordSpec {
   // I've done the first test for you!
   "TaxCalculator.calculateTax" should {
     "return the total amount of tax to pay" when {
-      "the income is below the personal tax limit" in {
-        val result: Double = taxCalculator.calculateTax(5000)
 
+      "the income is below the personal tax allowance" in {
+        val result: Double = taxCalculator.calculateTax(5000)
         assert(result == 0)
       }
+
+      "the income is above the personal tax allowance but below the basic rate limit" in {
+        val result: Double = taxCalculator.calculateTax(40000)
+        val basicTax: Int = 6000
+        assert(result == basicTax)
+      }
+
+      "the income is above the basic rate limit but below the higher rate limit" in {
+        val result: Double = taxCalculator.calculateTax(100000)
+        val basicTax: Int = 8000
+        val higherTax: Int = 20000
+        assert(result == basicTax + higherTax)
+      }
+
+      "the income is above the higher rate limit" in {
+        val result: Double = taxCalculator.calculateTax(200000)
+        val basicTax: Int = 8000
+        val higherTax: Int = 30000
+        val additionalTax: Int = 33750
+        assert(result == basicTax + higherTax + additionalTax)
+      }
+
     }
   }
 }
